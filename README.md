e8a4ea500a5e3b213176d2b4ca89e0a8582503d8
key-amb <yasutake.kiyoshi@gmail.com>
2015-05-05 14:17:27 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::Koyomi - It's new $module
+
+# SYNOPSIS
+
+    use App::Koyomi;
+
+# DESCRIPTION
+
+App::Koyomi is ...
+
+# LICENSE
+
+Copyright (C) YASUTAKE Kiyoshi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+YASUTAKE Kiyoshi <yasutake.kiyoshi@gmail.com>
---
+++ lib/App/Koyomi.pm
@@ -0,0 +1,39 @@
+package App::Koyomi;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Koyomi - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::Koyomi;
+
+=head1 DESCRIPTION
+
+App::Koyomi is ...
+
+=head1 LICENSE
+
+Copyright (C) YASUTAKE Kiyoshi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+YASUTAKE Kiyoshi E<lt>yasutake.kiyoshi@gmail.comE<gt>
+
+=cut
+
