772cbd58cf104ae0182bb90206af3861f8cbd3dc
key-amb <yasutake.kiyoshi@gmail.com>
2015-06-05 06:51:59 +0900

    Method to create Job

--- lib/App/Koyomi/DataSource/Job/Teng.pm
+++ lib/App/Koyomi/DataSource/Job/Teng.pm
@@ -86,6 +86,50 @@ sub get_by_id {
     );
 }
 
+sub create {
+    args(
+        my $self,
+        my $data => 'HashRef',
+        my $ctx  => 'App::Koyomi::Context',
+        my $now  => +{ isa => 'DateTime', optional => 1 },
+    );
+    $now ||= $ctx->now;
+    my $teng = $self->teng;
+
+    # Transaction
+    my $txn = $teng->txn_scope;
+
+    my $now_db = DateTime::Format::MySQL->format_datetime($now);
+    eval {
+        # create jobs
+        my %job = map { $_ => $data->{$_} } qw/user command memo/;
+        $job{created_on} = $job{updated_at} = $now_db;
+        my $new_job = $teng->insert('jobs', \%job);
+        unless ($new_job) {
+            croakf(q/Insert jobs Failed! data=%s/, ddf(\%job));
+        }
+
+        # create job_times
+        for my $t (@{$data->{times}}) {
+            my %time = (
+                job_id => $new_job->id,
+                %$t,
+                created_on => $now_db,
+                updated_at => $now_db,
+            );
+            $teng->insert('job_times', \%time)
+                or croakf(q/Insert job_times Failed! data=%s/, ddf(\%time));
+        }
+    };
+    if ($@) {
+        $txn->rollback;
+        die $@;
+    }
+
+    $txn->commit;
+    return 1;
+}
+
 sub update_by_id {
     args(
         my $self,
